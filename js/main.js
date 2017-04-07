538884565a7dcfc25b42f6e4f1d47355238e429d
addykumar27 <adarshna27@yahoo.com>
2017-04-07 13:26:33 -0700

    debugging existing function to get images to move

--- js/main.js
+++ js/main.js
@@ -1,28 +1,56 @@
 console.log('Running');
 //starting out with the two characters
+
 $(document).ready(function() {
     var $tom = $('#tom');
     var $jerry = $('#jerry');
+    var gameEnds = false;
+    var trackLength = true;
        
  //to move the image across the page
  // this function is not working
- $(document).keydown(function(key) {
- $(document).on('keydown', function(element){
- if(element.keyCode == 83) {
-   $('#tom').animate({
-    right: '-=10'
-    }, 0);
-   }
- })
- $(document).keydown(function(key) {
-   $(document).on('keydown', function(element){
-   if(element.keyCode == 76) {
-   $('#jerry').animate({
-   right: '-=10'
-   }, 0);
+  
+
+  // when start is pressed runners begin
+  $('#start').click(function(){
+  $('#start').toggle();
+  trackLength();
+});
+
+
+
+
+ $(document).keydown(function(event){
+ if(event.keyCode == 83) {
+   $('#tom').animate({'left':'+=10'},20)}
+   if(event.keyCode == 76) {
+   $('#jerry').animate({'left':'+=10'},20)}
+
+ //when tom or jerry reaches end of track first wins!!!
+  if($('#tom').css('left')=='750px') {
+    if(gameOver!==true) {
+      alert('tom wins! Time to eat jerry')
+      gameEnds=true;
+    }
   }
-})
+  if($('#jerry').css('left')=='750px') {
+    if(gameOver!==true) {
+      alert('jerry wins! Time for some cheese')
+      gameEnds=true;
+    }
+  }
+});
 
 
-         
+//for restarting the game
+$('#reset').click(function(){
+  $('#tom').animate({left:'0px'},0);
+  $('#jerry').animate({left:'0px'},0);  
+  gameEnds=false;
+  boardLength();
+});
+
+});
+
+
 
