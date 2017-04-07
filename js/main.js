63decffade16e45f4326fa376b1319a9ab39354b
addykumar27 <adarshna27@yahoo.com>
2017-04-06 21:17:27 -0700

    added function for moving the object across page.NOT WORKING

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
-
 console.log('Running');
+//starting out with the two characters
+$(document).ready(function() {
+    var $tom = $('#tom');
+    var $jerry = $('#jerry');
+       
+ //to move the image across the page
+ // this function is not working
+ $(document).keydown(function(key) {
+ $(document).on('keydown', function(element){
+ if(element.keyCode == 83) {
+   $('#tom').animate({
+    right: '-=10'
+    }, 0);
+   }
+ })
+ $(document).keydown(function(key) {
+   $(document).on('keydown', function(element){
+   if(element.keyCode == 76) {
+   $('#jerry').animate({
+   right: '-=10'
+   }, 0);
+  }
+})
+
+
+         
+
